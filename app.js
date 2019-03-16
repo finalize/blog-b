712a545fa0d07922cac11260cc0c562471cafeb6
shgysd <shgysd@gmail.com>
2019-03-16 23:59:26 +0900

    Add routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const tweet = require('../blog/src/server/routes/users');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/tweet', tweet);
 
 app.use('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'));
